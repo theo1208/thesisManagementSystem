71170797ec3ba5a56e23883d16f9665e94801456
OSzil <oszil@boozedump.hic>
2012-06-19 22:00:26 +0300

    Added CounilMember class for login purpouses..

---
+++ MppThesisServer/src/com/github/thesis/model/CouncilMember.java
@@ -0,0 +1,12 @@
+package com.github.thesis.model;
+
+
+/**
+ * @author Orban Szilard
+ *
+ */
+public class CouncilMember extends User {
+	public CouncilMember(String fullName, String userName, String password) {
+		super(fullName, userName, password);		
+	}	
+}
